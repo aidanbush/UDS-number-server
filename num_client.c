23dd92b8ff289705c1f0af41b986719df106bfbf
aidan <aidandbush@gmail.com>
2018-12-02 18:52:19 -0700

    Get client and server communicating

--- num_client.c
+++ num_client.c
@@ -7,9 +7,14 @@
  */
 
 /* standard libraries */
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <libgen.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef enum {
 	OP_NONE = 0,
@@ -18,13 +23,42 @@ typedef enum {
 	OP_RECV,
 } operation;
 
-void send_num(car *server, uint64_t num)
+void send_num(char *server, uint64_t num)
 {
+	int sfd, rd;
+	struct sockaddr_un sock = {0};
+	char buf[100];
+
+	if (strlen(server) >= sizeof(sock.sun_path)) {
+		return;
+	}
+
+	sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sfd == -1) {
+		perror("socket");
+		return;
+	}
+	sock.sun_family = AF_UNIX;
+
+	strcpy(sock.sun_path, server);
+
+	if (connect(sfd, (struct sockaddr*)&sock, sizeof(sock)) == -1) {
+		perror("connect");
+		return;
+	}
+
+	printf("server: %s, num: %ld\n", server, num);
+	// write msg
+	// read msg
+
+	close(sfd);
 }
 
-void recv_num(car *server)
+/*
+void recv_num(char *server)
 {
 }
+*/
 
 void print_usage(char *p_name)
 {
@@ -39,11 +73,15 @@ void print_usage(char *p_name)
 int main(int argc, char **argv)
 {
 	int c;
+	uint64_t num;
+	char *server;
 	operation op = OP_NONE;
 
-	while ((c = getopt(argc, argv, "h")) != -1) {
+	while ((c = getopt(argc, argv, "s:rh")) != -1) {
 		switch (c) {
 		case 's':
+			num = atoll(optarg);
+
 			if (op != OP_NONE)
 				op = OP_ERR;
 			else
@@ -64,8 +102,18 @@ int main(int argc, char **argv)
 		}
 	}
 
+	if (op == OP_STOR || op == OP_RECV) {
+		if (optind >= 1) {
+			server = argv[optind];
+		} else {
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
+
 	switch (op) {
 		case OP_STOR:
+			send_num(server, num);
 			// send_num
 			break;
 		case OP_RECV:
--- num_server.c
+++ num_server.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <stdint.h>
 
-#define BUF_LEN	12
+#define BACKLOG	5
 
 int server(char *sock_name)
 {
@@ -21,7 +21,7 @@ int server(char *sock_name)
 		return -1;
 	}
 
-	sfd = socket(AF_UNIX, SOCK_DGRAM, 0);
+	sfd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (sfd == -1) {
 		perror("socket");
 		return -1;
@@ -30,7 +30,6 @@ int server(char *sock_name)
 
 	unlink(sock_name);
 
-	sock.sun_family = AF_UNIX;
 	strcpy(sock.sun_path, sock_name);
 
 	if (bind(sfd, (struct sockaddr *)&sock, sizeof(sock)) < 0) {
@@ -39,26 +38,24 @@ int server(char *sock_name)
 		return -1;
 	}
 
-	close(sfd);
-	printf("exit");
-	return -1;
-
-	ssize_t plen;
-
-	struct sockaddr_un client_sock;
-	uint8_t buf[BUF_LEN];
-	socklen_t len;
+	//listen
+	if (listen(sfd, BACKLOG) < 0) {
+		perror("listen");
+		close(sfd);
+		return -1;
+	}
 
 	// loop on accept
-	while ((plen = recvfrom(sfd, buf, BUF_LEN, 0, (struct sockaddr *) &client_sock, &len)) >= 0) {
-		printf("recv");
+	while ((cfd = accept(sfd, (struct sockaddr *)&sock, &slen)) != -1) {
+		printf("accept\n");
+		close(cfd);
 		//save stuff into struct
 		//determine op code
 		//write to read
 		//send packet back
 	}
 
-	perror("recvfrom");
+	perror("accept");
 	close(sfd);
 	return 1;
 }
